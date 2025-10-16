ea0ef9c9f80d1d441dba571af95de7175ba16443
Hafiz Muhammad Moaz Paracha <n1nflexacc@gmail.com>
2025-10-15 17:49:16 -0700

    Updated navbar responsiveness and dropdown

--- src/components/navbar/GlobeDropdown.jsx
+++ src/components/navbar/GlobeDropdown.jsx
@@ -1,27 +1,57 @@
+import { useState } from "react";
 import { GlobeAltIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 
-const GlobeDropdown = () => (
-  <div className="flex items-center space-x-2">
-    <button className="p-2 rounded-full transform transition-transform duration-200 hover:scale-110">
-      <GlobeAltIcon className="w-5 h-5 text-gray-300 hover:text-white" />
-    </button>
+const GlobeDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    <div className="relative group">
-      <span className="text-sm text-white pb-5 cursor-pointer">▼</span>
-      <div className="fixed w-40 bg-white h-45 text-black shadow-lg rounded-lg
-        absolute transform -translate-x-1/2 text-center
-        opacity-0 invisible group-hover:opacity-100 group-hover:visible 
-        transition-all duration-300 z-40">
-        <div className="pt-4">
-          <Link to="/Mea" className="block text-xl font-semibold hover:text-orange-700">MEA</Link>
-          <Link to="/Ksa" className="block text-xl font-semibold hover:text-orange-700 mt-3">KSA</Link>
-          <Link to="/Apac" className="block text-xl font-semibold hover:text-orange-700 mt-3">APAC</Link>
-          <Link to="/Global" className="block text-xl font-semibold hover:text-orange-700 mt-3">Global</Link>
-        </div>
+  return (
+    <div
+      className="relative globe-dropdown inline-block"
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+    >
+      {/* Globe + Arrow (Attached, No Background) */}
+      <div className="flex items-center space-x-2 p-2 rounded-md cursor-pointer">
+        <GlobeAltIcon className="w-5 h-5 text-gray-300 transition" />
+        <span className="text-sm text-white">▼</span>
       </div>
+
+      {/* Dropdown Menu */}
+      {isOpen && (
+        <div
+          className="absolute right-0 top-full mt-1 w-40 bg-white text-black shadow-lg rounded-lg z-50 text-center"
+        >
+          <div className="py-3">
+            <Link
+              to="/Mea"
+              className="block text-sm font-semibold py-2 hover:text-orange-700"
+            >
+              MEA
+            </Link>
+            <Link
+              to="/Ksa"
+              className="block text-sm font-semibold py-2 hover:text-orange-700"
+            >
+              KSA
+            </Link>
+            <Link
+              to="/Apac"
+              className="block text-sm font-semibold py-2 hover:text-orange-700"
+            >
+              APAC
+            </Link>
+            <Link
+              to="/Global"
+              className="block text-sm font-semibold py-2 hover:text-orange-700"
+            >
+              Global
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 export default GlobeDropdown;
